76eb502ae400ec0a9165b5fb7dec6ec5ee6ed62d
Yan Maeda <Yan Maeda>
2016-09-10 12:54:22 -0500

    Modified practice - Else

--- Else.java
+++ Else.java
@@ -2,10 +2,15 @@ class Else
 {
 	public static void main(String[]args)
 	{
-		int hrs = 11;
-		if(hrs<13)
+		int hrs = 18;
+		if ( hrs < 13 )
 		{
 			System.out.println("Good morning: " + hrs);
 		}
+		else if ( hrs < 18 )
+		{
+			System.out.println( "Good afternoon: " + hrs ) ;
+		}
+		else System.out.println( "Good evening: " + hrs ) ;
 	}
 }
